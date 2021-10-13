36c9c93321664a504f6830b2281294c619978568
XavierCoulon <xcoulon.mail@gmail.com>
2021-10-13 17:41:56 +0200

    Add functions to extract all categories, to create csv file by category, to download pics

--- main.py
+++ main.py
@@ -2,8 +2,51 @@ import csv
 import re
 import requests
 from pprint import pprint
+from pathlib import Path
 from text_to_num import text2num
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+URL = "http://books.toscrape.com/"
+
+
+def extract_categories():
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, "html.parser")
+    categories_list = []
+    categories_tags = soup.find_all("a", href=re.compile("/category/books/"))
+    for tag in categories_tags:
+        categories_list.append(URL + tag["href"])
+    print(f"{len(categories_list)} catégories répertoriées.")
+
+    return categories_list
+
+
+def extract_books_from_category(category_url_index):
+    r = requests.get(category_url_index)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    category_name = soup.find(class_="page-header action").find("h1").string
+
+    # Seek for the number of additional pages to scrap
+    number_additional_pages = int(soup.find(class_="form-horizontal").find("strong").string) // 20
+
+    # Initialize list of category's urls, adding index
+    category_url_list = [category_url_index]
+    for i in range(number_additional_pages):
+        category_url_list.append(category_url_index.replace("index.html", f"page-{i + 2}.html"))
+    pprint(f"{len(category_url_list)} pages to be extracted in {category_name} (including index.html).")
+
+    # Initialize list of books in the category
+    category_books_list = []
+    for category_url in category_url_list:
+        print(f"{category_url} in progress...")
+        r = requests.get(category_url)
+        soup = BeautifulSoup(r.content, "html.parser")
+        for tag in soup.find_all(href=re.compile("index"), title=True):
+            category_books_list.append(tag["href"].replace("../../..", "http://books.toscrape.com/catalogue"))
+    print(f"{len(category_books_list)} books found in {category_name}.")
+
+    return category_books_list
 
 
 def extract_book_information(book_page_url):
@@ -47,28 +90,71 @@ def extract_book_information(book_page_url):
     book_information["category"] = category
 
     # Extract Product Description
-    product_description = soup.find("p", class_="").string
-    book_information["product_description"] = product_description
+    product_description = soup.find("p", class_="")
+    # if tag not found / to implement for all extracts ?
+    # replace ; by . to avoid Excel issue in csv file - to be checked
+    book_information["product_description"] = product_description.string.replace(";", ".") if product_description else ""
 
     # Extract Image URL
     image_url = soup.find(alt=title).attrs["src"]
+    image_url = image_url.replace("../../", URL)
     book_information["image_url"] = image_url
+
+    # Download image
+    download_pic(image_url, title.replace("/", "-"))
+
+    # pprint(f"{title} extracted")
 
     return book_information
 
 
-def create_csv_file(book_information):
-    file_name = "data.csv"
-    with open(file_name, "w", newline="") as output_file:
-        writer = csv.DictWriter(output_file, fieldnames=book_information.keys())
+def create_csv_file(category_books_list, file_name):
+    p = Path.cwd()
+    p = p / "CSV"
+    p.mkdir(exist_ok=True)
+    file_name = f"{file_name}.csv"
+    file_to_open = p / file_name
+    headers = ["UPC", "product_page_url", "price_excluding_tax", "price_including_tax", "number_available", "title",
+               "review_rating", "category", "product_description", "image_url"]
+    with open(file_to_open, "w", newline="") as output_file:
+        writer = csv.DictWriter(output_file, fieldnames=headers)
         writer.writeheader()
-        writer.writerow(book_information)
-        print("Fichier créé.")
+        writer.writerows(category_books_list)
+        print(f"Fichier {file_name} créé sous /CSV.")
+
+
+def download_pic(image_url, file_name):
+    p = Path.cwd()
+    p = p / "IMG"
+    p.mkdir(exist_ok=True)
+    image_name = f"{file_name}.jpg"
+    file_to_create = p / image_name
+    image = requests.get(image_url).content
+    with open(file_to_create, "wb") as handler:
+        handler.write(image)
 
 
 def main():
-    book = extract_book_information("http://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html")
-    create_csv_file(book)
+    categories_list = extract_categories()
+    nb_books = 0
+    start_time = datetime.now()
+    for category_url_index in categories_list:
+        category_all_books_information = []
+        category_books = extract_books_from_category(category_url_index)
+        for book in category_books:
+            category_all_books_information.append(extract_book_information(book))
+        nb_books += len(category_all_books_information)
+        create_csv_file(category_all_books_information, category_all_books_information[0]["category"])
+    print(f"{nb_books} books found.")
+    print(datetime.now() - start_time)
 
 
 main()
+
+# pprint(extract_book_information("http://books.toscrape.com/catalogue/shtum_733/index.html"))
+# liste = []
+# liste.append(extract_book_information("http://books.toscrape.com/catalogue/shtum_733/index.html"))
+# pprint(liste)
+# create_csv_file(liste, "test")
+
+# download_pic("https://books.toscrape.com/media/cache/63/62/63623a0b014b1f26e49aa61786e6e708.jpg", "test")
