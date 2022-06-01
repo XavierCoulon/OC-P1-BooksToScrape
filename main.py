c6a85d87ef99a803dd78de957b119031bf547915
XavierCoulon <xcoulon.mail@gmail.com>
2022-06-01 15:25:30 +0200

    multiprocessing implemented

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
+from module import scrape_categories, scrape_book_data, scrape_books_from_category, create_csv_file
 from multiprocessing import Pool
-
-from module import *
 from datetime import datetime
 from tqdm import tqdm
-import os
 
 
 def main():
@@ -24,10 +21,14 @@ def main():
     start_time = datetime.now()
 
     for category_url_index in scrape_categories():
-        category_all_books_data = []
+        # category_all_books_data = []
         category_books = scrape_books_from_category(category_url_index)
-        for book in tqdm(category_books):
-            category_all_books_data.append(scrape_book_data(book))
+        p = Pool()
+        category_all_books_data = p.map(scrape_book_data, tqdm(category_books))
+
+        # for book in tqdm(category_books):
+        #    category_all_books_data.append(scrape_book_data(book))
+
         nb_books_scraped += len(category_all_books_data)
         create_csv_file(category_all_books_data, category_all_books_data[0]["category"])
 
@@ -35,41 +36,5 @@ def main():
     print(datetime.now() - start_time)
 
 
-def test(url):
-    print(url)
-
-
 if __name__ == "__main__":
-    # p = Pool()
-    # result = p.map(scrape_books_from_category, scrape_categories())
-    #
-    # print(len(result))
-    # p.close()
-    # p.join()
-
-    nb_books_scraped = 0
-    start_time = datetime.now()
-
-    for category_url_index in scrape_categories():
-        category_books = scrape_books_from_category(category_url_index)
-        pprint(category_books)
-        with Pool() as p:
-            category_all_books_data = p.map(scrape_book_data, category_books)
-            print(category_all_books_data)
-            p.close()
-            p.join()
-
-        #nb_books_scraped += len(category_all_books_data)
-        #create_csv_file(category_all_books_data, category_all_books_data[0]["category"])
-
-    #print(f"{nb_books_scraped} books extracted.")
-    #print(datetime.now() - start_time)
-
-
-
-
-
-
-
-
-# main()
+    main()
